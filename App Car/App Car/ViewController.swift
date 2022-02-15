8ff41700ef4fa5cb00451a06e170bff28792b076
acepele <47071340+acepele@users.noreply.github.com>
2022-02-14 21:41:55 -0600

    Add files via upload

---
+++ App Car/App Car/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  App Car
+//
+//  Created by student on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var SelectionBtn: UIButton!
+    
+    @IBOutlet var CategoryItems: [UIButton]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        
+    }
+    
+    
+    
+    @IBAction func Selection(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func Categories(_ sender: UIButton) {
+    }
+}
+
+
